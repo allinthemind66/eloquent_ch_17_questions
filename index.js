22d6497e79f290f272878ade51630cce98b3810e
allinthemind66 <r.nilon92@gmail.com>
2018-08-20 16:53:22 -0400

    create star pattern

--- index.js
+++ index.js
@@ -35,17 +35,35 @@
 // slantedLines(10)
 
 //Spiral
-function drawSpiral(){
-  let cx = document.querySelector("canvas").getContext("2d");
-  cx.beginPath();
-  let angle = Math.PI / 2;
-  let radius = 10;
-  for(let i = 0; i < 300; i++){
-    angle += -16 * .01;
-    radius += .3
-    cx.lineTo(100 + Math.cos(angle) * radius, 100 + Math.sin(angle) * radius)
-  }
-  cx.stroke();
-};
+// function drawSpiral(){
+//   let cx = document.querySelector("canvas").getContext("2d");
+//   cx.beginPath();
+//   let angle = Math.PI / 2;
+//   let radius = 10;
+//   for(let i = 0; i < 300; i++){
+//     angle += -16 * .01;
+//     radius += .3
+//     cx.lineTo(100 + Math.cos(angle) * radius, 100 + Math.sin(angle) * radius)
+//   }
+//   cx.stroke();
+// };
+//
+// drawSpiral()
 
-drawSpiral()
+//Star Shape
+
+function drawStar(x, y){
+  let cx = document.querySelector("canvas").getContext("2d");
+  let radius = 50, xCenter = x + radius, yCenter = y + radius;
+  cx.beginPath();
+  cx.moveTo(xCenter + radius, yCenter)
+  for(let i = 0; i <= 8; i++){
+    let angle = i * Math.PI / 4;
+    cx.quadraticCurveTo(xCenter, yCenter, xCenter + Math.cos(angle) * radius,
+          yCenter + Math.sin(angle) * radius)
+      //equation for a point on a circle is the center plus angle
+  }
+  cx.fillStyle = 'gold'
+  cx.fill();
+};
+drawStar(50,50)
