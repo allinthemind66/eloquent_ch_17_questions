d32d62dd6b77f8005baa16437ab88fd8ef8981c8
allinthemind66 <r.nilon92@gmail.com>
2018-08-21 15:13:23 -0400

    create labels for pie chart

--- index.js
+++ index.js
@@ -52,18 +52,59 @@
 
 //Star Shape
 
-function drawStar(x, y){
-  let cx = document.querySelector("canvas").getContext("2d");
-  let radius = 50, xCenter = x + radius, yCenter = y + radius;
-  cx.beginPath();
-  cx.moveTo(xCenter + radius, yCenter)
-  for(let i = 0; i <= 8; i++){
-    let angle = i * Math.PI / 4;
-    cx.quadraticCurveTo(xCenter, yCenter, xCenter + Math.cos(angle) * radius,
-          yCenter + Math.sin(angle) * radius)
-      //equation for a point on a circle is the center plus angle
+// function drawStar(x, y){
+//   let cx = document.querySelector("canvas").getContext("2d");
+//   let radius = 50, xCenter = x + radius, yCenter = y + radius;
+//   cx.beginPath();
+//   cx.moveTo(xCenter + radius, yCenter)
+//   for(let i = 0; i <= 8; i++){
+//     let angle = i * Math.PI / 4;
+//     cx.quadraticCurveTo(xCenter, yCenter, xCenter + Math.cos(angle) * radius,
+//           yCenter + Math.sin(angle) * radius)
+//       //equation for a point on a circle is the center plus angle
+//   }
+//   cx.fillStyle = 'gold'
+//   cx.fill();
+// };
+// drawStar(50,50)
+
+//############## QUESTION 2
+
+const results = [
+  {name: "Satisfied", count: 1043, color: "lightblue"},
+  {name: "Neutral", count: 563, color: "lightgreen"},
+  {name: "Unsatisfied", count: 510, color: "pink"},
+  {name: "No comment", count: 175, color: "silver"}
+];
+
+let cx = document.querySelector("canvas").getContext("2d");
+  let total = results
+    .reduce((sum, {count}) => sum + count, 0);
+  let currentAngle = -0.5 * Math.PI;
+  let centerX = 300, centerY = 150;
+
+  // Add code to draw the slice labels in this loop.
+  for (let result of results) {
+    let sliceAngle = (result.count / total) * 2 * Math.PI;
+    cx.beginPath();
+    cx.arc(centerX, centerY, 100,
+           currentAngle, currentAngle + sliceAngle);
+     let middleAngle = currentAngle + 0.5 * sliceAngle;
+     let textX = Math.cos(middleAngle) * 120 + centerX;
+     let textY = Math.sin(middleAngle) * 120 + centerY;
+     console.log(middleAngle)
+     console.log(Math.cos(middleAngle))
+     cx.textBaseline = 'middle'
+     if(Math.cos(middleAngle) > 0){
+       cx.textAlign = 'left'
+     } else {
+       cx.textAlign = 'right'
+     }
+     cx.font = "15px sans-serif";
+    cx.fillStyle = "black";
+     cx.fillText(result.name, textX, textY)
+    currentAngle += sliceAngle;
+    cx.lineTo(centerX, centerY);
+    cx.fillStyle = result.color;
+    cx.fill();
   }
-  cx.fillStyle = 'gold'
-  cx.fill();
-};
-drawStar(50,50)
